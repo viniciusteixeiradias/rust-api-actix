c185a313bd6beb88c5f6498a738584d10d0c1996
viniciusteixeiradias <viniciustdias2001@gmail.com>
2022-09-30 07:36:05 -0300

    Create database and insert

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use actix_web::*;
-
 mod routes;
 use routes::ping::*;
 use routes::info::*;
@@ -23,3 +22,56 @@ async fn main() -> Result<(), std::io::Error> {
     api.run()
     .await
 }
+
+// fn main() -> Result<(), Error> {
+//     let mut client = Client::connect(
+//         "postgresql://dboperator:operatorpass123@localhost:5243/postgres",
+//         NoTls,
+//     )?;
+
+//     // Create Table
+//     client.batch_execute(
+//         "
+//         CREATE TABLE IF NOT EXISTS users (
+//             id              SERIAL PRIMARY KEY,
+//             username        VARCHAR UNIQUE NOT NULL,
+//             password        VARCHAR NOT NULL,
+//             email           VARCHAR UNIQUE NOT NULL
+//             )
+//     ",
+//     )?;
+//     println!("Create Database");
+
+//     // Insert
+//     client.execute(
+//         "INSERT INTO users (username, password, email) VALUES ($1, $2, $3)",
+//         &[&"user2", &"mypass2", &"use2@gmail.com"],
+//     )?;
+//     client.execute(
+//         "INSERT INTO users (username, password, email) VALUES ($1, $2, $3)",
+//         &[&"user3", &"anotherpass", &"mister3@test.com"],
+//     )?;
+//     println!("Insert Data");
+
+//     // Update
+//     // client.execute(
+//     //     "UPDATE app_user SET username=$1 WHERE id=$2",
+//     //     &[&"jack1", &2],
+//     // )?;
+
+//     // Delete
+//     // client.execute("DELETE FROM app_user WHERE id=$1", &[&1])?;
+//     // client.execute("DELETE FROM app_user WHERE id=$1", &[&3])?;
+
+//     Ok(())
+// }
+
+// for row in client.query("SELECT id, username, password, email FROM app_user", &[])? {
+//     let id: i32 = row.get(0);
+//     let username: &str = row.get(1);
+//     let password: &str = row.get(2);
+//     let email: &str = row.get(3);
+//     println!(
+//         "found app user: {}) {} | {} | {}",
+//         id, username, password, email
+// );
